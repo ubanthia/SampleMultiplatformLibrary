b39fb0b81821cf75b8bb7ed737da4ea6df55f9ca
ubanthia <ubanthia@microsoft.com>
2023-07-19 13:05:11 +0530

    Initial Commit

--- src/androidMain/kotlin/com.example.multiplatformpoc/Platform.kt
+++ src/androidMain/kotlin/com.example.multiplatformpoc/Platform.kt
@@ -1,5 +1,9 @@
 package com.example.multiplatformpoc
 
+import android.annotation.TargetApi
+import android.os.Build
+
+@TargetApi(Build.VERSION_CODES.DONUT)
 class AndroidPlatform : Platform {
     override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
 }
